5fc2c8ca5b61bd6d5d488c18e36ef329b3717a3b
Fernando Matsuo Santos <fernando.matsuo@moduscreate.com>
2021-09-22 22:45:32 -0300

    uncomment terraform secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 # This is a security test
 # Never expose your access_key or secret_key
-#provider "aws" {
-#access_key = "AKIAWNQSRZEWJPUI3RJK"
-#secret_key = "xy9ZzM6TWLlgJ52Hs1UGwLL5xE2Ms6EJX/KELKLd"
-#region     = "us-east-1"
-#}
+provider "aws" {
+access_key = "AKIAWNQSRZEWJPUI3RJK"
+secret_key = "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"
+region     = "us-east-1"
+}
 
 resource "aws_s3_bucket" "bucket-demo" {
   bucket = "fms-bucket-demo-259624"
