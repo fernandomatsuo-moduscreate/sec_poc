76c1a75b08916961cb05fbeb8b23043e75c071f1
Fernando Matsuo Santos <fernando.matsuo@moduscreate.com>
2021-09-30 15:01:00 -0300

    remove ruby

--- ruby_vulnerable/db/seeds.rb
+++
@@ -1,9 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Article.connection.execute("insert into users values(1, 'admin@test.com', '3f786850e387550fdab836ed7e6dc881de23001b', NOW(), NOW()) ")
-Article.create(title: "Hello World", text: "Content of our first post")
